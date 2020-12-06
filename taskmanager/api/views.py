bb3586177f4eda684d000e42aa55d70a5b747fcd
attiliogiglio <attilio.g93@gmail.com>
2020-12-05 22:24:25 -0300

    listing completed todos API

--- taskmanager/api/views.py
+++ taskmanager/api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+# Not use because there is no rendering, just an API
+# from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics, permissions
+from .serializers import TodoSerializer
+from todo.models import Todo
+
+class TodoCompletedList(generics.ListAPIView):
+    serializer_class = TodoSerializer
+    permission_class = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        # filter by an specific logged user and with data on datecompleted
+        return Todo.objects.filter(user=user, datecompleted__isnull=False).order_by('-datecompleted')
+
+
+
