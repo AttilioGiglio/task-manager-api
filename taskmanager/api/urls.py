ac6e3cb7e8d037d5ead9625c60517eb84f343283
attiliogiglio <attilio.g93@gmail.com>
2020-12-05 22:55:48 -0300

    create, retrieve, update, destroy - todos

--- taskmanager/api/urls.py
+++ taskmanager/api/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('todos', views.TodoCreateList.as_view()),
+    path('todos/<int:pk>', views.TodoEditList.as_view()),
     path('todos/completed', views.TodoCompletedList.as_view()),
 ]
--- taskmanager/api/views.py
+++ taskmanager/api/views.py
@@ -15,4 +15,25 @@ class TodoCompletedList(generics.ListAPIView):
         return Todo.objects.filter(user=user, datecompleted__isnull=False).order_by('-datecompleted')
 
 
+class TodoCreateList(generics.ListAPIView):
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        # filter by an specific logged user and with no data on datecompleted
+        return Todo.objects.filter(user=user, datecompleted__isnull=True)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class TodoEditList(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        # filter by an specific logged user
+        return Todo.objects.filter(user=user)
 
