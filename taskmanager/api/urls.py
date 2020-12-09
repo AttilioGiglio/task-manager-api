429fd9a58b82ae16193f2018a83a8d3df43c106d
attiliogiglio <attilio.g93@gmail.com>
2020-12-09 15:58:50 -0300

    adding token api

--- taskmanager/api/urls.py
+++ taskmanager/api/urls.py
@@ -3,8 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # todos
     path('todos', views.TodoCreateList.as_view()),
     path('todos/<int:pk>', views.TodoEditList.as_view()),
     path('todos/<int:pk>/complete', views.TodoComplete.as_view()),
     path('todos/completed', views.TodoCompletedList.as_view()),
+    #auth
+    path('signup', views.signup),
+    path('login', views.login)
 ]
